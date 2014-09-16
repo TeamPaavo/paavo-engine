f75ec7aa103c117f9613ca344dd24e0e336f6bda
Tomi Kortelainen <tomi.kortelainen@gmail.com>
2014-09-16 10:55:40 +0300

    Update main.cpp

--- Sample/src/main.cpp
+++ Sample/src/main.cpp
@@ -3,17 +3,10 @@
 int main()
 {
 	pv::Window win;
-	win.create(L"Ötsikkö", 300, 300);
+	win.create(L"Ã–tsikkÃ¶", 300, 300);
 
-	pv::Window win2;
-	win2.create(L"Ötsikkö2", 300, 600);
+	while (win.update()){
 
-	while (win.isOpen() || win2.isOpen())
-	{
-		if (win.isOpen())
-			win.update();
-		if (win2.isOpen())
-			win2.update();
 	}
 
 	return 0;
